1be1c416253513048332c5f04c923df554f0e680
Tim Dumol <tim@timdumol.com>
2025-07-20 19:37:05 +0200

    Fix broken gitignore

---
+++ frontend/src/lib/hooks.ts
@@ -0,0 +1,5 @@
+
+export function useSuggestTopics() {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return { suggestions: [], fetchSuggestions: async (_topic: string) => void 0 }
+}
